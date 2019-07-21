04b2279c3e58aca4c93ad0d4cab9027298f49c77
Wennder dos Santos <wennder.santos@outlook.com>
2019-07-21 11:18:36 -0300

    Fixed sized hashmap instead of array

--- strings/maxoccurringchar/maxoccurringchar.go
+++ strings/maxoccurringchar/maxoccurringchar.go
@@ -1,8 +1,8 @@
 package maxoccurringchar
 
 //MaxOccurringChar executes in
-//Time: O(n) where n is the length of the input
-//Memory: O(1) - Always alocating [256]int,
+//Time: O(n) where n is the length of the input.
+//Memory: O(1) - Always alocating a fixed size hashmap
 //doesn't matter length of n
 func MaxOccurringChar(text string) string {
 	result := ""
@@ -11,11 +11,15 @@ func MaxOccurringChar(text string) string {
 		return result
 	}
 
-	memory := [256]int{}
+	memory := make(map[rune]int, 62)
 	max := 0
 
 	for _, char := range text {
-		memory[char]++
+		if _, ok := memory[char]; ok {
+			memory[char]++
+		} else {
+			memory[char] = 1
+		}
 
 		if memory[char] > max {
 			result = string(char)
